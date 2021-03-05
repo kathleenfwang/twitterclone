30b4e3bb93a10d47badf14ba84a02c1c9625d0e4
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-03-05 12:35:27 -0800

    add constructor for context and tweets in TweetsAdapter

--- app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetsAdapter.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,9 +9,24 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
 import org.w3c.dom.Text;
 
+import java.util.List;
+
 public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+
+    Context context;
+    List<Tweet> tweets;
+    // generate constructor to populate member variables
+
+    public TweetsAdapter(Context context, List<Tweet> tweets) {
+        // this refers to "this", otherwise if we set context = context we get error
+        this.context = context;
+        this.tweets = tweets;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
